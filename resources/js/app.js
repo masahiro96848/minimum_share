f6de206c9c91e61f96eb3cc082477c8e3f2a0885
vagrant <vagrant@homestead>
2020-08-10 01:57:17 +0000

    vue インストール

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,32 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
 
-// $(function () {
+window.Vue = require('vue');
 
-//   // フロートヘッダーメニュー
-//   var targetHeight = $('.js-float-menu-target').height();
-//   $($window).on('scroll', function(){
-//     $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
-//   });
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
 
-//   // spメニュー
-//   $('js-toggle-sp-menu').on('click', function(){
-//     $(this).toggleClass('active');
-//     $('.js-toggle-sp-menu-target').toggleClass('active');
-//   });
-// });
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-$('.p-menu--button').on('click', function() {
-  if( $(this).hasClass('active')){
-    $(this).removeClass('active');
-    $('.p-menu--wrap').addClass('close').removeClass('open');
-  }else {
-    $(this).addClass('active');
-    $('.p-menu--wrap').addClass('open').removeClass('close');
-  }
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
 });
